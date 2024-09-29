821fc5f42da7e17765bc3345f27993a2a7a8d0ec
Phuoc Le <huuphuoc2791@gmail.com>
2024-09-29 13:39:34 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Variables for registry and image names
-REGISTRY_URL=your-registry-url
+
+REGISTRY_URL=xxxxxx.dkr.ecr.us-east-1.amazonaws.com
 FRONTEND_IMAGE=frontend-app
 BACKEND_IMAGE=backend-app
+
+# Go to the source code of the application
+cd application
 
 # Build backend image
 docker build -f Dockerfile.backend -t $REGISTRY_URL/$BACKEND_IMAGE .
 
 # Build frontend image
-docker build -f Dockerfile.frontend -t $REGISTRY_URL/$FRONTEND_IMAGE .
+docker build -f  Dockerfile.frontend -t $REGISTRY_URL/$FRONTEND_IMAGE .
 
 # Push to registry
 docker push $REGISTRY_URL/$BACKEND_IMAGE
